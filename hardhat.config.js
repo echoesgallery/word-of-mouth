5c8cd95f9f3cf2876347d07628ac08e9a196f63e
V G <vgotcheva@flex5>
2021-10-10 09:56:59 -0400

    set up private keys for deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,37 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require('@nomiclabs/hardhat-ethers');
+require("@nomiclabs/hardhat-etherscan");
+
+// private keys for rinkeby testnet
+const { alchemyAPIKey, ethereumAccount, etherscanAPIKey} = require('./.secrets.json');
+
+// private keys for polygon mainnet
+//const { alchemyAPIKey, ethereumAccount, etherscanAPIKey} = require('./.secrets-polygon.json');
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      
+      // On https://dashboard.alchemyapi.io/ display the key with "View Key" button
+      url: alchemyAPIKey,
+      // In methamask, choose "Account details", then "Export Private Key".
+      accounts: [ethereumAccount]
+    },
+    polygon: {
+      
+      // On https://dashboard.alchemyapi.io/ display the key with "View Key" button
+      url: alchemyAPIKey,
+      // In methamask, choose "Account details", then "Export Private Key".
+      accounts: [ethereumAccount]
+    }
+  },
+  etherscan: {
+    apiKey: etherscanAPIKey
+  }
 };
